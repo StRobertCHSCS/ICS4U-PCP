450d4e4d594da4a5ca5c1e1356e50d337f565404
Justin Cho <Justin Cho>
2016-04-14 12:04:44 -0400

    Trying to make a background

--- Game.py
+++ Game.py
@@ -1,13 +1,27 @@
 __author__ = "Justin", "Danny"
 
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty,\
-    ObjectProperty
-from kivy.vector import Vector
-from kivy.clock import Clock
+import kivy
+import random
+import sys
+kivy.require("1.8.0")
 
+from kivy.properties import NumericProperty, ReferenceListProperty, BooleanProperty, ObjectProperty, ListProperty
+from kivy.uix.image import Image
+from kivy.vector import Vector
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.config import Config
+from kivy.core.window import Window
+from kivy.uix.widget import Widget
+
+
+class Background(Widget):
+    background = ObjectProperty(Image())
 
 class NameApp(App):
     def build(self):
-        pass
+        game = Background()
+        Clock.schedule_interval(game.update, 1.0/60.0)
+        return game
+
+NameApp().run()
