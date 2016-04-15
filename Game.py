fce369c3b5616e37f0e366fee3f9a17b5e34f80f
Justin Cho <Justin Cho>
2016-04-15 10:48:18 -0400

    Tried to make backgournd move, failed

--- Game.py
+++ Game.py
@@ -1,9 +1,10 @@
 __author__ = "Justin", "Danny"
 
 import kivy
+kivy.require("1.8.0")
+
 import random
 import sys
-kivy.require("1.8.0")
 
 from kivy.properties import NumericProperty, ReferenceListProperty, BooleanProperty, ObjectProperty, ListProperty
 from kivy.uix.image import Image
@@ -17,12 +18,23 @@ from kivy.uix.widget import Widget
 
 class Background(Widget):
     background = ObjectProperty(Image())
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+
+    def move(self):
+        self.x = self.x + self.velocity_x
+        self.y = self.y + self.velocity_y
+
+    def update(self, dt):
+        self.move()
+
 
 
 class NameApp(App):
     def build(self):
-        return Background()
-
+        game = Background()
+        Clock.schedule_interval(game.update, 1.0 / 60.0)
+        return game
 
 if __name__ == "__main__":
     NameApp().run()
