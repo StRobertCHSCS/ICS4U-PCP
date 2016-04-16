4b7bb56096cf290e66b14ddef22a85dd07066450
QifanGuo <danny_gu0@hotmail.com>
2016-04-16 15:53:40 -0400

    got the bird to fall, not yet fly

--- Game.py
+++ Game.py
@@ -39,38 +39,37 @@ class Background(Widget):
         self.image1.pos = (0, 0)
         self.image2.pos = (self.width, 0)
 
-class PlayerObj(Widget):
-    #placeholder image used
-    player_image = ObjectProperty(Image())
+class PlayerObj(Image):
 
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
-
+    source = "images/flappynormal.png"
     def __init__(self, **kwargs):
+        self.image = ObjectProperty(Image())
         super(PlayerObj, self).__init__(**kwargs)
+        self.velocity_y = 0
+        self.gravity = -0.5
 
-"""
     def on_touch_down(self, touch):
-        self.jumping = True
-        self.bird_image.source = "images/flappynormal.png"
-        self.velocity_y = self.jump_height / (self.jump_time * 60.0)
-        Clock.unschedule(self.stop_jumping)
-        Clock.schedule_once(self.switch_to_normal, self.jump_time  / 5.0)
-"""
+        self.velocity_y += 3
+
+    def update(self):
+        self.velocity_y += self.gravity
+        self.velocity_y = max(self.velocity_y, -1.5)
+        self.y += self.velocity_y
+
+
 
 class Game(Widget):
     background = ObjectProperty(Background())
-    player = ObjectProperty(PlayerObj())
 
-    #change window size below, can get rid of black stuff tho
-    Window.size = (800, 600)
 
     def __init__(self, **kwargs):
         super(Game, self).__init__(**kwargs)
         self.background.velocity = [-1, 0]
         self.bind(size=self.size_callback)
+        self.player = PlayerObj(pos = (self.width / 4, self.height/2))
+        self.add_widget(self.player)
 
+        Clock.schedule_interval(self.update, 1.0/60.0)
 
     def size_callback(self, instance, value):
         self.background.size = value
@@ -78,9 +77,8 @@ class Game(Widget):
 
     def update(self, dt):
         self.background.update()
-        """
         self.player.update()
-        """
+
 
 class NameApp(App):
     def build(self):
