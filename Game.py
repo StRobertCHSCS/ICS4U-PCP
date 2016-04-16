68d33cb3edfebab621dce587700a10e605d3fe4e
QifanGuo <danny_gu0@hotmail.com>
2016-04-16 13:43:40 -0400

    start to the flappy bird, not yet displayed tho

--- Game.py
+++ Game.py
@@ -16,6 +16,8 @@ from kivy.core.window import Window
 from kivy.uix.widget import Widget
 
 
+
+
 class Background(Widget):
     image1 = ObjectProperty(Image())
     image2 = ObjectProperty(Image())
@@ -37,14 +39,38 @@ class Background(Widget):
         self.image1.pos = (0, 0)
         self.image2.pos = (self.width, 0)
 
+class PlayerObj(Widget):
+    #placeholder image used
+    player_image = ObjectProperty(Image())
+
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def __init__(self, **kwargs):
+        super(PlayerObj, self).__init__(**kwargs)
+
+"""
+    def on_touch_down(self, touch):
+        self.jumping = True
+        self.bird_image.source = "images/flappynormal.png"
+        self.velocity_y = self.jump_height / (self.jump_time * 60.0)
+        Clock.unschedule(self.stop_jumping)
+        Clock.schedule_once(self.switch_to_normal, self.jump_time  / 5.0)
+"""
 
 class Game(Widget):
     background = ObjectProperty(Background())
+    player = ObjectProperty(PlayerObj())
+
+    #change window size below, can get rid of black stuff tho
+    Window.size = (800, 600)
 
     def __init__(self, **kwargs):
         super(Game, self).__init__(**kwargs)
         self.background.velocity = [-1, 0]
         self.bind(size=self.size_callback)
+
 
     def size_callback(self, instance, value):
         self.background.size = value
@@ -52,14 +78,15 @@ class Game(Widget):
 
     def update(self, dt):
         self.background.update()
-
+        """
+        self.player.update()
+        """
 
 class NameApp(App):
     def build(self):
         game = Game()
         Clock.schedule_interval(game.update, 1.0 / 100.0)
         return game
-
 if __name__ == "__main__":
     NameApp().run()
 
