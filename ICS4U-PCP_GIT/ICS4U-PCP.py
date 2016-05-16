74eab0f404a68e4c2d0ee1372da03447d0b655d0
Aaron Choi <aaronchoi@Aarons-MacBook-Pro.local>
2016-05-16 13:36:10 -0400

    create word and get user input

--- ICS4U-PCP_GIT/ICS4U-PCP.py
+++ ICS4U-PCP_GIT/ICS4U-PCP.py
@@ -1,135 +1,185 @@
-import curses
-from random import randint
-from curses import wrapper
-import time
+# SNAKES GAME
+# Use ARROW KEYS to play, SPACE BAR for pausing/resuming and Esc Key for exiting
+# https://gist.githubusercontent.com/sanchitgangwar/2158089/raw/5f3d0003801acfe1a29c4b24f2c8975efacf6f66/snake.py
 
-mydebugOn = True
+import curses
+from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
+from random import randint
+import time
+import threading
+
+ent = ""
+test_val = 1
+SCR_Y_MAX = 40
+SCR_X_MAX = 60
+
+def my_raw_input(stdscr, r, c, prompt_string):
+    curses.echo()
+    stdscr.addstr(r, c, prompt_string)
+    stdscr.refresh()
+    input = stdscr.getstr(r + 1, c, 20)
+    return input  #       ^^^^  reading input at next line
+
+def clear_line(y):
+  """ (int) -> None
+  """
+  empty = " " * SCR_X_MAX
+  win.addstr(y, 1, empty)
+
+def debug(msg, level):
+  """ (str, int, int) -> None
+  """
+  y = SCR_Y_MAX - 10 + level
+  clear_line(y)
+  #win.addstr(y, 1, 'DEBUG: '+msg)
+
+def move_down(screen, word):
+  if word == None:
+    return
+  empty = " " * len(word[2])
+  screen.addstr(word[0], word[1], empty)
+  word[0] += 1
+  screen.addstr(word[0], word[1], word[2])
+
+def create_work():
+
+  word_len = 4
+  word = ""
+  for ch_num in range(word_len):
+    ch = chr(randint(ord('a'), ord('z')))
+    word += ch
+  x = randint(0, SCR_X_MAX)
+  y_x_word = [ 0, x, word ]
+  #debug("new word: "+str(y_x_word), 2)
+  return y_x_word
+
+def create_word(screen, word):
+  while 1:
+    #screen.border(0)
+    global test_val
+    global ent
+
+
+    if word:
+      screen.addstr(12, 12, 'testval:'+str(test_val));
+      move_down(screen, word)
+      #screen.addstr(12, 12, time.strftime("%a, %d %b %Y %H:%M:%S"))
+      screen.refresh()
+
+      if ent == word[2]:
+        global curses
+        curses.beep()
+        word = None
+        word = create_work()
+    time.sleep(0.5)
+
 stdscr = curses.initscr()
+curses.noecho()
+curses.curs_set(0)
 stdscr.clear()
+#stdscr.resize(50, 50)
 stdscr.border(0)
 stdscr.refresh()
-print "hello"
+#choice = my_raw_input(stdscr, 2, 3, "cool or hot?").lower()
+#print("CHOICE: " +str(choice))
+#choice = my_raw_input(stdscr, 2, 3, "cool or hot?").lower()
 
-def debug(mystr):
-  if mydebugOn:
-    stdscr.addstr(30,30,"Debug:" + str(mystr))
+#win = curses.newwin(SCR_Y_MAX, SCR_X_MAX, 0, 0)
 
-
-def optimize_screen():
-  """
-  Check user's screen and send error mesage
-  :return: bool
-  """
-  # get user's screen
-  stdscr.refresh()
- # width = curses.COLS -1
- # length = curses.LINES -1
-
-  length,width = stdscr.getmaxyx()
-
-  # check the screen size,
-  # if width or length is less than the rquired size, send error message
-
-  REQ_W = 112
-  REQ_L = 46
-
-  if width < REQ_W or length < REQ_L:
-    debug(stdscr.getmaxyx)
-    #stdscr.addstr(9,3,str(stdscr.getmaxyx()))
-    stdscr.addstr(10,3,str(width) + " " + str(length))
-    stdscr.addstr(20,45,"Please make your screen size"+str(REQ_W)+"x"
-                  +str(REQ_L))
-    stdscr.addstr(21,45,"Press spacebar to continue")
-
-    test = stdscr.getch()
+#win.keypad(1)
+#curses.noecho()
+#curses.curs_set(0)
+#win.border(0)
 
 
-    return False
+#key = KEY_RIGHT                                                    # Initializing values
+#curses.delay_output(1000)
+#curses.echo()
 
-  return True
+new_word = create_work()
+#win.border(0)
 
-
-
-
-def coordinates(stdscr):
-  first_point = 0
-  end_point = curses.COLS -1
-  tenth_coord = 0
-
-  while first_point < end_point:
-    if first_point % 10 == 0:
-        stdscr.addstr(1,first_point,str(tenth_coord))
-        tenth_coord += 1
-
-    stdscr.addstr(2,first_point,str(first_point%10))
-    first_point += 1
-
-  first_point_2 = 0
-  vertical_end_point = curses.LINES -1
-  tenth_coord_2 = 0
-
-  while first_point_2 < vertical_end_point:
-    if first_point_2 % 10 == 0:
-      stdscr.addstr(first_point_2,1,str(tenth_coord_2))
-      tenth_coord_2 += 1
+#---------------------------------------
+#win = curses.newwin(10,10,0,0)
+#win.border(0)
+#win.addstr(0,0,'hELLo')
+#win.refresh()
+#---------------------------------------
 
 
-    stdscr.addstr(first_point_2,2,str(first_point_2%10))
-    first_point_2 += 1
+# pad ----------------------------------------
+#pad = curses.newpad(100, 100)
+##  These loops fill the pad with letters; this is
+## explained in the next section
+#for y in range(0, 100):
+#    for x in range(0, 100):
+#        try:
+#            pad.addch(y,x, ord('a') + (x*x+y*y) % 26)
+#        except curses.error:
+#            pass
 
+#  Displays a section of the pad in the middle of the screen
+#pad.refresh(0,0, 5,5, 20,75)
+#win.getch()
 
-def main(stdscr):
-  # check user's screen size
-  is_small_screen = True
-  #while is_small_screen:
-  #  #test
-  #  is_small_screen =not optimize_screen()
-  #  stdscr.addstr(7,2,str(is_small_screen))
-  #  stdscr.getch()
+#win2 = curses.newwin(5,5,41,00)
+#
+#while key != 27:
+#    win2.border(0)
+#    win2.refresh()
+#
+#    win.border(0)
+#    win.refresh()
+#    #win.timeout(500)
+#    #prevKey = key
+#    #win.getch()
+#
+#    ## no key is pressed
+#    #if event == -1:
+#    #    key = key
+#    #else:
+#    #    key = event
+#
+#    #debug('key: ' + str(key) + ' ' +
+#    #      'chr(' + str(key) + '): ' + str(chr(key)), 1)
+#    move_down(new_word)
+#    time.sleep(0.5)
+#    #choice = my_raw_input(stdscr, 2, 3, "cool or hot?").lower()
+#    #print("CHOICE: " +str(choice))
 
-  # make screen
-  stdscr.clear()
-  stdscr.refresh()
+clock = threading.Thread(target=create_word, args=(stdscr,new_word))
+clock.daemon = True
+clock.start()
+saved =""
+while 1:
+    #time.sleep(1)
+    #new_word = create_work()
+    #clock2 = threading.Thread(target=create_word, args=(stdscr,new_word))
+    #clock2.daemon = True
+    #clock2.start()
 
-  # debug only
-  coordinates(stdscr)
-  # load first page
-  first_screen(stdscr)
-  #debug("test")
-  stdscr.getch()
-
-  """
-  while 1:
+    #time.sleep(1)
+    global test_val
+    test_val += 1
     event = stdscr.getch()
-    stdscr.addstr(10,10,str(event))
-    stdscr.addstr(12,10,chr(int(event)))
-    stdscr.addstr(5,10,str("Press spacebar to continue!"))
+    if event != 10:
+      saved += chr(event)
+    if event == 8:
+      stdscr.addstr(15,10, 'backspace')
+      saved = saved[:-2]
+    stdscr.addstr(10,10, '                                                          ')
+    stdscr.addstr(10,10, 'saved:['+str(saved) + '] event: ' +str(event))
+    if event == 10:
+      global ent
+      ent = saved
+      saved = ""
+      stdscr.addstr(10,10, 'saved:['+str(saved) + '] event: ' +str(event))
+    #if event == ord("q"):
     if event == 27:
-      break
-
-    coordinates(stdscr)
-  """
-
-  """
-  if event == 32:
-      first_screen(stdscr)
-  """
+        break
 
 
-def first_screen(stdscr):
-
-  #                          V  H
-  title_pad = curses.newpad(10,10)
-
-
-  title_pad.addstr(2,5,"boarder")
-  title_pad.refresh(1,2,5,10,15,20)
-
-  debug("test")
-
-  stdscr.refresh()
 
 curses.endwin()
-
-
-wrapper(main)
+#print("\nScore - " + str(score))
+print("http://bitemelater.in\n")
