1feb98d85c066ac325c2075e0ef5e4bce7ed0d2b
Bonnie Xu <bonnie.xuu@gmail.com>
2016-06-11 18:03:12 -0400

    Set variables

--- app/src/main/java/com/app/bonnie/bliss_countdown/MainActivity.java
+++ app/src/main/java/com/app/bonnie/bliss_countdown/MainActivity.java
@@ -1,13 +1,28 @@
 package com.app.bonnie.bliss_countdown;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView txtTimerDay, txtTimerHour, txtTimerMinute, txtTimerSecond;
+    private TextView event_here;
+    private Handler handler;
+    private Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtTimerDay = (TextView) findViewById(R.id.txtTimerDay);
+        txtTimerHour = (TextView) findViewById(R.id.txtTimerHour);
+        txtTimerMinute = (TextView) findViewById(R.id.txtTimerMinute);
+        txtTimerSecond = (TextView) findViewById(R.id.txtTimerSecond);
+        event_here = (TextView) findViewById(R.id.event_here);
+        countDownStart();
+    }
+
+    public void countDownStart() {
     }
 }
