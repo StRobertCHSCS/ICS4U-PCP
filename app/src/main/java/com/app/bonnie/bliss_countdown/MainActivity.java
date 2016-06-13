fa47c4e18a0ce7bfa02e19dd19dfc3ace171e469
Bonnie Xu <bonnie.xuu@gmail.com>
2016-06-12 22:20:50 -0400

    Finalized code, countdown works

--- app/src/main/java/com/app/bonnie/bliss_countdown/MainActivity.java
+++ app/src/main/java/com/app/bonnie/bliss_countdown/MainActivity.java
@@ -3,13 +3,19 @@ package com.app.bonnie.bliss_countdown;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+@SuppressWarnings("ConstantConditions")
 public class MainActivity extends AppCompatActivity {
     private TextView txtTimerDay, txtTimerHour, txtTimerMinute, txtTimerSecond;
     private TextView event_here;
     private Handler handler;
-    private Runnable runnable;
+    protected Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +30,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void countDownStart() {
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                handler.postDelayed(this, 1000);
+                try {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat(
+                            "yyyy-MM-dd", Locale.CANADA);
+// Please here set your event date//YYYY-MM-DD
+                    Date futureDate = dateFormat.parse("2016-06-30");
+                    Date currentDate = new Date();
+                    if (!currentDate.after(futureDate)) {
+                        long diff = futureDate.getTime()
+                                - currentDate.getTime();
+                        long days = diff / (24 * 60 * 60 * 1000);
+                        diff -= days * (24 * 60 * 60 * 1000);
+                        long hours = diff / (60 * 60 * 1000);
+                        diff -= hours * (60 * 60 * 1000);
+                        long minutes = diff / (60 * 1000);
+                        diff -= minutes * (60 * 1000);
+                        long seconds = diff / 1000;
+                        txtTimerDay.setText("" + String.format("%02d", days));
+                        txtTimerHour.setText("" + String.format("%02d", hours));
+                        txtTimerMinute.setText("" + String.format("%02d", minutes));
+                        txtTimerSecond.setText("" + String.format("%02d", seconds));
+                    } else {
+                        event_here.setVisibility(View.VISIBLE);
+                        event_here.setText(R.string.yourevent);
+                        textViewGone();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        handler.postDelayed(runnable, 1000);
+    }
+    public void textViewGone() {
+        findViewById(R.id.LinearLayout10).setVisibility(View.GONE);
+        findViewById(R.id.LinearLayout11).setVisibility(View.GONE);
+        findViewById(R.id.LinearLayout12).setVisibility(View.GONE);
+        findViewById(R.id.LinearLayout13).setVisibility(View.GONE);
+        findViewById(R.id.event_here).setVisibility(View.GONE);
     }
 }
