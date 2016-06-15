c086404acada8f78ef3c00932c46945e2169b8c8
Kovenwei <koven.wei17@ycdsbk12.ca>
2016-06-14 20:41:00 -0400

    Added imports

--- mixtape_jump.py
+++ mixtape_jump.py
@@ -1,1 +1,11 @@
-"I HOPE THIS WORKS I WANT IT TO WORk"
+import kivy
+kivy.require('1.7.2')
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.core.window import Window
+from kivy.properties import NumericProperty
+from kivy.clock import Clock
+from kivy.graphics import Rectangle, Color, Canvas
+from random import *
