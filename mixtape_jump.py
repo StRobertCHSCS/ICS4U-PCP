cfd37d16e7f9ec436fd3368cbcf439aeb443185c
Kovenwei <koven.wei17@ycdsbk12.ca>
2016-06-14 20:54:03 -0400

    Created class GUI(widget) initialized the rapper amount in list initialized the score initialized the probability of spawn
    Given __init__
    created score label
    created title
    specified location of image player

--- mixtape_jump.py
+++ mixtape_jump.py
@@ -10,6 +10,21 @@ from kivy.clock import Clock
 from kivy.graphics import Rectangle, Color, Canvas
 from random import *
 
-"I HOPE THIS WORKS I WANT IT TO WORk"
-"Imma send you some text"
-#John is a goon
+class GUI(Widget):
+    rapperList =[]
+    rapperScore = NumericProperty(0)
+    minProb = 1780
+    def __init__(self, **kwargs):
+        super(GUI, self).__init__(**kwargs)
+        self.score = Label(text = "0")
+        self.score.y = Window.height*0.8
+        self.score.x = Window.width*0.2
+        self.rapperScore = 0
+        l = Label(text='Mixtape Jump')
+        l.x = Window.width/2 - l.width/2
+        l.y = Window.height*0.8
+        self.add_widget(l)
+        self.mixtape = mixtape(imageStr = 'C:\Users\Koven\desktop\photoshopped\mmixtape.png')
+        self.mixtape.x = Window.width/4
+        self.mixtape.y = Window.height/2
+        self.add_widget(self.mixtape)
