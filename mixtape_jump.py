538cb9be8c604d23a71430ee839207f408b23e7c
Kovenwei <koven.wei17@ycdsbk12.ca>
2016-06-14 21:22:36 -0400

    acidentally added yutaro's

--- mixtape_jump.py
+++ mixtape_jump.py
@@ -35,31 +35,6 @@ class GUI(Widget):
         self.add_widget(self.score)
 
 
-class WidgetDrawer(Widget):
 
-    def __init__(self, imageStr, **kwargs):
-        super(WidgetDrawer, self).__init__(**kwargs)
-        with self.canvas:
-
-            self.size = (Window.width*.005*25,Window.height*.005*25)
-            self.rect_bg= Rectangle(source=imageStr,pos=self.pos,size = self.size)
-
-            self.bind(pos=self.update_graphics_pos)
-            self.x = self.center_x
-            self.y = self.center_y
-
-            self.pos = (self.x,self.y)
-
-            self.rect_bg.pos = self.pos
-
-    def update_graphics_pos(self, instance, value):
-
-        self.rect_bg.pos = value
-
-    def setSize(self,width, height):
-        self.size = (width, height)
-    def setPos(self,xpos,ypos):
-        self.x = xpos
-        self.y = ypos
 
 
