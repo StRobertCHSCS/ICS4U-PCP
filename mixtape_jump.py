219b4cf78ea42a45c20142ccf45e7a798b2c1ff1
John Python <junghun.han16@ycdsbk12.ca>
2016-06-15 08:42:20 +0800

    imports

--- mixtape_jump.py
+++ mixtape_jump.py
@@ -1,1 +1,11 @@
-"Imma send you some text"
+import kivy
+kivy.require('1.7.2')
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.core.window import Window
+from kivy.properties import NumericProperty
+from kivy.clock import Clock
+from kivy.graphics import Rectangle, Color, Canvas
+from random import *
