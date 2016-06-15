71d3467766334c1275a048ab0de9e743aee81adf
Kovenwei <koven.wei17@ycdsbk12.ca>
2016-06-14 20:47:35 -0400

    Merge remote-tracking branch 'remotes/origin/master' into Koven-Wei
    # Conflicts:
    #	mixtape_jump.py

--- mixtape_jump.py
+++ mixtape_jump.py
@@ -1,3 +1,13 @@
-"I HOPE THIS WORKS I WANT IT TO WORk"
-"Imma send you some text"
-#John is a goon
+import kivy
+kivy.require('1.7.2')
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.core.window import Window
+from kivy.properties import NumericProperty
+from kivy.clock import Clock
+from kivy.graphics import Rectangle, Color, Canvas
+from random import *
+
+
