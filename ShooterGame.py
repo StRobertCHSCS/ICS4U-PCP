d68e0d97fc6f392e12ac41fff67d04dd863ecd31
Andy Park <Andy Park>
2016-06-16 17:39:52 -0400

    Adding files / Drawing in more images onto the screen.

--- ShooterGame.py
+++ ShooterGame.py
@@ -12,20 +12,43 @@ Created on:        06/16/2016
 import pygame
 #from pygame.locals import *
 
-# nitialize the game
+# Initialize the game
 pygame.init()
-width, height = 1200, 800
+width, height = 1200, 600
 screen=pygame.display.set_mode((width, height))
 
-# Load images
-player = pygame.image.load("image/commando.png").convert_alpha()
+### Load images
+
+# The character that the player will control
+player = pygame.image.load("image/man.png").convert_alpha()
 player = pygame.transform.scale(player, (100, 100))
 
-# Loop until game is completed
+# The wooden tiles on the bakcground
+background = pygame.image.load("image/background.png")
+background = pygame.transform.scale(background, (100, 100))
+
+# The girls that the player must protect
+objective = pygame.image.load("image/girl.png")
+objective = pygame.transform.scale(objective, (90, 90))
+
+### Loop until game is completed
 while 1:
 
     # Clear the screen
     screen.fill(0)
+
+    # Draw the background and objective
+    for x in range(width/background.get_width()+1):
+        for y in range(height/background.get_height()+1):
+            screen.blit(background,(x*100,y*100))
+
+    # Draw the objective (girls)
+    screen.blit(objective, (0,10))
+    screen.blit(objective, (0,110))
+    screen.blit(objective, (0,210))
+    screen.blit(objective, (0,310))
+    screen.blit(objective, (0,410))
+    screen.blit(objective, (0,510))
 
     # Draw the various objects onto the screen
     screen.blit(player, (50,50))
