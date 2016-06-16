dcb646235ddd6329eea22c8339cbb5fe6175a592
Ryan <Ryan>
2016-06-16 19:30:47 -0400

    Created WidgetDrawer class, draws all other widgets on the screen

--- game.py
+++ game.py
@@ -20,3 +20,31 @@ from kivy.config import Config
 Config.set('graphics','resizable',0) #prevents the app from being resizable
 
 Window.clearcolor = (0,0,0,1.)
+
+
+#create classes
+
+class WidgetDrawer(Widget):
+    #this draws all other widgets on the screen
+    #includes movement, size, positioning
+    #when a WidgetDrawer object is created, an image string is required
+    def __init__(self, imageStr, **kwargs):
+        super(WidgetDrawer, self).init(**kwargs)
+        with self.canvas:
+            self.size = (Window.width * .002 * 25, Window.width * .002 * 25)
+            self.rect_bg = Rectangle(source=imageStr, pos=self.pos, size=self.size)
+            self.bind(pos=self.update_graphics_pos)
+            self.x = self.center_x
+            self.y = self.center_y
+            self.pos = (self.x, self.y)
+            self.rect_bg.pos = self.pos
+
+            def update_graphics_pos(self, instance, value):
+                self.rect_bg.pos = value
+
+            def setSize(self, width, height):
+                self.size = (width, height)
+
+            def setPos(xpos, ypos):
+                self.x = xpos
+                self.y = ypos
