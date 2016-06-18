4e5008475ba18b4dc144156332525b1da7faf16b
Ryan <Ryan>
2016-06-18 15:32:59 -0400

    Various classes created

--- main.py
+++ main.py
@@ -2,5 +2,46 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty,ObjectProperty
 from kivy.vector import Vector
+from random import *
 from kivy.clock import Clock
+
+
+class Paddle(Widget):
+    score = NumericProperty(0)
+
+    def bounce(self, ball):
+        if self.collide_widget(ball):
+            vx, vy = ball.velocity
+            offset = (ball.center_y - self.center_y) / (self.height / 2)
+            bounced = Vector(-1 * vx, vy)
+            vel = bounced * 1.1
+            ball.velocity = vel.x, vel.y + offset
+
+class Ball(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def __init__(self):
+        # image properties
+        self.source = "images/Ball.png"
+        self.size = (60, 60)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+
+class Game(Widget):
+    ball = ObjectProperty(None)
+
+    def update(self, dt):
+        self.ball.move()
+
+        if (self.ball.y < 0) or (self.ball.top > self.height):
+            self.ball.velocity_y *= -1
+
+        if (self.ball.x < 0) or (self.ball.right > self.width):
+            self.ball.velocity_x *= -1
+
+
+
 
